04e7de5676b81729911bda2aec3b066e5356ca7d
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-06-03 08:29:29 +0300

    Added CurrencyRow Component for conversion between different currencies and styling for the simple currency convertor.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+  }, []);
   return (
-    <h1>Hello React</h1>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className='equals'>=</div>
+      <CurrencyRow />
+    </>
   );
 }
 
